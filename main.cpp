b5c1228c68fb0c3109b591fa26bfe20dfb6a8ef9
218302 <218302@student.pwr.edu.pl>
2016-04-20 17:50:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 
 #include <iostream>
 #include <stdio.h>
+// atoi()
+#include <stdlib.h>
 
 #include "Slav.h"
 
@@ -33,6 +35,9 @@ int main(int argc, char const *argv[])
 
 	containers(slavs, n);
 	adapters(slavs, n);
+	
+	// Slownik mesko-zenski Slowian.
+	create_slav_sex_map(slavs, n);
 
 	delete [] slavs;
 }
@@ -48,21 +53,32 @@ void containers(Slav * slavs, int n)
 	printf("## vector\n");
 
 	// Umieść Słowian w losowej kolejności w wektorze.
-
+	srand(time(NULL));
+	for(int i=0; i<n; i++)
+		if(rand()%2) vectorOfSlavs.push_back(&slavs[i]);
+		else vectorOfSlavs.insert(vectorOfSlavs.begin(), &slavs[i]);
 	// Wykorzystując iterator i funkcję description(), wyświetl wszystkich Słowian w wektorze
+	for(vector<Slav*>::iterator i=vectorOfSlavs.begin(); i!=vectorOfSlavs.end(); i++)
+		cout << (*i)->description() << endl;
 
 	REPORT_CONTAINERS;
 	printf("## set\n");
 
 	// Przenieś wszystkich Słowian z wektoru do zbioru.
+	for(vector<Slav*>::iterator i=vectorOfSlavs.begin(); i!=vectorOfSlavs.end(); i++)
+		setOfSlavs.insert(*i);
+	vectorOfSlavs.clear(); // Czyszczenie wektora.
 	
 	REPORT_CONTAINERS;
 	printf("## map\n");
 
 	// Stwórz słownik tworzący pary Słowian, z tych znajdujących się w zbiorze, czyszcząc zbiór
-	
+	for(set<Slav*>::iterator i=setOfSlavs.begin(); i!=setOfSlavs.end(); i++)
+		mapOfSlavs[*i]=*(++i);
+	setOfSlavs.clear();
 	// Wykorzystując iterator, wyświetl wszystkie pary Słowian
-	
+	for(map<Slav*, Slav*>::iterator i=mapOfSlavs.begin(); i!=mapOfSlavs.end(); i++)
+		cout << i->first->description() << "\t" << i->second->description() << endl;
 	REPORT_CONTAINERS;
 }
 
@@ -76,17 +92,46 @@ void adapters(Slav * slavs, int n)
 	printf("## queue\n");
 
 	// Umieść Słowian w kolejce.
+	for(int i=0; i<n; i++)
+		queueOfSlavs.push(&slavs[i]);
 	
 	REPORT_ADAPTERS;
 
 	printf("## stack\n");
 	// Przenieś Słowian z kolejki do stosu.
+	for(int i=0; i<n; i++)
+	{
+		stackOfSlavs.push(queueOfSlavs.front());
+		queueOfSlavs.pop();
+	}
 
 	REPORT_ADAPTERS;
 
 	// Wyświetl Słowian zdejmowanych ze stosu.
+	for(int i=0; i<n; i++)
+	{
+		cout << stackOfSlavs.top()->description() << endl;
+		stackOfSlavs.pop();
+	}
 
 	REPORT_ADAPTERS;
+}
+
+// Slownik mesko-zenski Slowian.
+void create_slav_sex_map(Slav* slavs, int n)
+{
+	map <vector<Slav*>, vector<Slav*>> slav_sex_map;
+	vector<Slav*> slav_males, slav_females;
+	for(int i=0; i<n; i++)
+	{
+		if(slavs[i].sex()==Slav::male)
+			slav_males.push_back(&slavs[i]);
+		else
+			slav_females.push_back(&slavs[i]);
+	}
+	slav_sex_map[slav_males]=slav_females;
+	// Sprawdzenie poprawnosci dzialania.
+	//printf("[slav_males = %lu, slav_females = %lu, slav_sex_map = %lu, existingSlavs = %i]\n", slav_males.size(), slav_females.size(), slav_sex_map.size(), Slav::counter());
 }
 
 void showMeContainerSizes(vector <Slav *> vector, set <Slav *> set, map <Slav *, Slav*> map)
