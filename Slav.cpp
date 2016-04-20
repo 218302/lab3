17a10f714afb6e720cebf2f13b74b6629a452a99
218302 <218302@student.pwr.edu.pl>
2016-04-20 17:53:25 -0500

    Update Slav.cpp

--- Slav.cpp
+++ Slav.cpp
@@ -1,6 +1,12 @@
 #include "Slav.h"
 #include <fstream>
 
+// czas i rand()
+#include <time.h>
+#include <stdlib.h>
+// istream_iterator
+#include <iterator>
+
 using namespace std;
 
 vector <string> Slav::names;
@@ -19,6 +25,10 @@ void Slav::init()
 Slav::Slav()
 {
 	static int amountOfNames = (init(), names.size());
+/*
+inicjalizacja zmiennej statycznej w konstruktorze klasy - wykonanie funkcji init(),
+a pozniej przypisanie zmiennej wartosci names.size() (leniwa inicjalizacja)
+*/
 	_name = names[rand() % amountOfNames];
 	_id = _counter++;
 }
@@ -26,4 +36,12 @@ Slav::Slav()
 string Slav::description()
 {
 	return string("  ") + _name + " [" + to_string(_id) + "]";
+}
+
+Slav::sex_enum Slav::sex()
+{
+	if(_name[_name.size()-1]=='a')
+		return female;
+	else
+		return male;
 }
